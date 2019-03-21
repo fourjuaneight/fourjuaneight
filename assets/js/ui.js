a8ec0d2e80497d7fe85eb0a08c5cf20daf17de14
Juan Villela <juan@villela.co>
2019-03-21 16:23:02 -0400

    Reverting left hand functionality due to bad performance.

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,51 +1,13 @@
 const body = document.querySelector(`body`);
-const ui = document.getElementById(`ui-options`);
 const sets = document.getElementById(`settings`);
 const ops = document.querySelector(`section.options`);
 const colors = document.querySelectorAll(`p.colors`);
 const fonts = document.querySelectorAll(`p.type`);
-const leftHand = document.getElementById(`left-handed`);
-let count = 0;
 
-window.onload = () => {
-  if (localStorage.getItem(`cache`) === `visited`) {
-    leftHand.classList.add(`none`);
-  }
-};
-
-sets.addEventListener(
-  `click`,
-  () => {
-    count += 1;
-    ops.classList.toggle(`active`);
-    sets.classList.toggle(`active`);
-    leftHand.classList.toggle(`active`);
-    if (count > 3) {
-      localStorage.setItem(`cache`, `visited`);
-    }
-    if (localStorage.getItem(`cache`) === `visited`) {
-      leftHand.classList.add(`none`);
-    }
-  },
-  false
-);
-
-if (!localStorage.getItem(`side`)) {
-  leftHand.addEventListener(`click`, () => {
-    leftHand.classList.add(`none`);
-    leftHand.classList.remove(`active`);
-    localStorage.setItem(`side`, `left`);
-    ops.classList.remove(`active`);
-    sets.classList.remove(`active`);
-    ui.classList.add(`left`);
-  });
-}
-
-if (localStorage.getItem(`side`) === `left`) {
-  const side = localStorage.getItem(`side`);
-  ui.setAttribute(`class`, side);
-  leftHand.classList.add(`none`);
-}
+sets.addEventListener(`click`, () => {
+  ops.classList.toggle(`active`);
+  sets.classList.toggle(`active`);
+});
 
 const uiSwitcher = (item, change, mod) => {
   item.forEach(option => {
@@ -54,11 +16,10 @@ const uiSwitcher = (item, change, mod) => {
       () => {
         const value = option.id;
         body.setAttribute(mod, value);
-        leftHand.classList.remove(`active`);
+        sets.classList.remove(`active`);
+        ops.classList.remove(`active`);
         localStorage.removeItem(change);
         localStorage.setItem(change, value);
-        ops.classList.remove(`active`);
-        sets.classList.remove(`active`);
       },
       false
     );
